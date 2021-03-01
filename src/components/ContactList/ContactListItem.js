bb420ea6e1b282e2c4166be1850981d5c331fa7e
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-03-01 23:29:57 +0200

    Divider test

--- src/components/ContactList/ContactListItem.js
+++ src/components/ContactList/ContactListItem.js
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -23,6 +24,7 @@ export default function ContactListItem({ id, name, number, onDeleteContact }) {
       <CardContent>
         <span>{name}:</span> {number}
       </CardContent>
+      <Divider />
       <CardActions>
         <IconButton
           color="primary"
